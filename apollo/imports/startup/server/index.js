978ad76ebf7cfee9f9410e8e45b4ec81b88d962e
Adam Mathieson <mathieson1222@gmail.com>
2020-01-02 15:26:17 -0800

    adds a schema (typeDefs, resolvers)

--- apollo/imports/startup/server/index.js
+++ apollo/imports/startup/server/index.js
@@ -1,7 +1,27 @@
 import { createApolloServer } from 'meteor/apollo'
 import { makeExecutableSchema } from 'graphql-tools'
 
-// const { ApolloServer, gql} = require('apollo-server')
+// const { ApolloServer, gql} = require('apollo-server') // needs dep download
 // const server = new ApolloServer({})
 
-createApolloServer({})
+// graphql syntax, not JS
+const typeDefs =  `
+type Query {
+    hi: String
+}
+`
+
+const resolvers = {
+    Query: {
+        hi() {
+            return "Hello Level Up"
+        }
+    }
+}
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+})
+
+createApolloServer({ schema })
