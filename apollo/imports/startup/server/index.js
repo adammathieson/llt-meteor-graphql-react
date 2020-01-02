a314a4cf233f5a94df62569f034044102e26ff31
Adam Mathieson <mathieson1222@gmail.com>
2020-01-02 15:13:14 -0800

    revert apollo v2.0.0 for the createApolloServer() syntax to work

---
+++ apollo/imports/startup/server/index.js
@@ -0,0 +1,7 @@
+import { createApolloServer } from 'meteor/apollo'
+import { makeExecutableSchema } from 'graphql-tools'
+
+// const { ApolloServer, gql} = require('apollo-server')
+// const server = new ApolloServer({})
+
+createApolloServer({})
