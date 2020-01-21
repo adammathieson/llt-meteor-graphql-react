6e2a991424b1f97af8c8ab2f7449874b54794f46
Adam Mathieson <mathieson1222@gmail.com>
2020-01-21 15:27:28 -0800

    extend schema types; file cleanup

--- apollo/imports/startup/server/register-api.js
+++ apollo/imports/startup/server/register-api.js
@@ -6,34 +6,14 @@ import ResolutionsSchema from '../../api/resolutions/Resolutions.graphql'
 import ResolutionResolvers from '../../api/resolutions/resolvers'
 import UsersResolvers from '../../api/users/resolvers'
 import UsersSchema from '../../api/users/User.graphql'
-// hi
-const testSchema = `
-type Query {
-    hi: String
-    resolutions: [Resolution]
-    user: User 
-}
-`
-
-// const { ApolloServer, gql} = require('apollo-server') // needs dep download
-// const server = new ApolloServer({})
+// hii
 
 const typeDefs =  [
-    testSchema,
     ResolutionsSchema,
     UsersSchema,
 ]
 
-const testResolvers = {
-    Query: {
-        hi() {
-            return "Hello Level Up";
-        }
-    }
-}
-
 const resolvers = merge(
-    testResolvers,
     ResolutionResolvers,
     UsersResolvers,
 )
